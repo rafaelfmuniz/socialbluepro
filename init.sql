ca4d3e1cfcb1415a84be94bb06098e87d4546dae
rafaelfmuniz <rafaelfmuniz@gmail.com>
2026-01-30 13:45:17 -0700

    cleanup: remove unnecessary directories and files

--- init.sql
+++
@@ -1,4 +0,0 @@
-ALTER USER postgres WITH PASSWORD 'socialbluepro_pass';
-CREATE USER socialbluepro WITH PASSWORD 'socialbluepro_pass';
-CREATE DATABASE socialbluepro OWNER socialbluepro;
-GRANT ALL PRIVILEGES ON DATABASE socialbluepro TO socialbluepro;
